46ffeedf9d241d1a3fdb80c028baab23c04e9de1
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-10 22:58:55 -0400

    create layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+export default Layout;
