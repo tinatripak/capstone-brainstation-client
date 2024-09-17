84b9567c05ad1510de81cbf1401fbcc815c8a54e
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-16 23:04:50 -0400

    created a signup page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,25 @@
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import authBg from "../../assets/images/auth-bg.png";
 import "./SignUp.scss";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="sign-up">
+      <div className="sign-up__wrapper">
+        <img className="sign-up__bg-image" src={authBg} alt="Auth bg" />
+        <div className="sign-up__content">
+          <h1 className="sign-up__heading">Sign Up</h1>
+          <SignUpForm />
+          <div className="sign-up__login">
+            <p>Do you have an account?</p>
+            <a href="/login" className="sign-up__login-link">
+              Sign In
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
