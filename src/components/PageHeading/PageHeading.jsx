926505794e556d9f4a36e52947870fc10c54e5e9
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-16 23:08:00 -0400

    created a page heading with h1 and p

--- src/components/PageHeading/PageHeading.jsx
+++ src/components/PageHeading/PageHeading.jsx
@@ -1,7 +1,14 @@
 import "./PageHeading.scss";
 
-const PageHeading = () => {
-  return <div>PageHeading</div>;
+const PageHeading = ({ text1, text2 }) => {
+  return (
+    <div className="page-heading">
+      <h1 className="page-heading__content">{text1}</h1>
+      <p className="page-heading__content page-heading__content--second">
+        & <span className="page-heading__border-bottom">{text2}</span>
+      </p>
+    </div>
+  );
 };
 
 export default PageHeading;
