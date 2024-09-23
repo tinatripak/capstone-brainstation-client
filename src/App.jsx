4f3d96adfe924fd8577ece2ecc9915b41cdc41fd
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-22 20:52:20 -0400

    added scrolltotop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
@@ -8,10 +8,22 @@ import AboutUs from "./pages/AboutUs/AboutUs";
 import Poetry from "./pages/Poetry/Poetry";
 import PoetryItem from "./pages/PoetryItem/PoetryItem";
 import AccountPanel from "./components/AccountPanel/AccountPanel";
+import { useEffect } from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
