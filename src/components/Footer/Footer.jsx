c8c1141594d68f73b7e383f4949b2fe2d8c31575
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-24 00:04:14 -0400

    added a responsive design for a footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import NavLogo from "../NavLogo/NavLogo";
 import NavMenu from "../NavMenu/NavMenu";
 import "./Footer.scss";
@@ -6,7 +7,30 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__content">
-        <NavMenu />
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            `footer__link  ${isActive ? "footer__link--active" : ""}`
+          }
+        >
+          HOME
+        </NavLink>
+        <NavLink
+          to={"/poetry"}
+          className={({ isActive }) =>
+            `footer__link  ${isActive ? "footer__link--active" : ""}`
+          }
+        >
+          POETRY
+        </NavLink>
+        <NavLink
+          to={"/about"}
+          className={({ isActive }) =>
+            `footer__link  ${isActive ? "footer__link--active" : ""}`
+          }
+        >
+          ABOUT US
+        </NavLink>
         <NavLogo />
       </div>
 
