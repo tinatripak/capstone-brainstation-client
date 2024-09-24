97798fcf3a9d4e613489fed55f4d295054a044da
Kristina Tripak <tinatripak2002@gmail.com>
2024-09-24 00:05:37 -0400

    added a responsive design for a nav menu

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,14 +1,43 @@
 import { NavLink } from "react-router-dom";
-import "./NavMenu.scss";
 import { useCookies } from "react-cookie";
 import { useEffect, useState } from "react";
 import { checkToken, getUserById } from "../../scripts/auth-api";
 import defaultUserPhoto from "../../assets/images/default-user-photo.jpg";
+import "./NavMenu.scss";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const NavMenu = () => {
   const [cookies, removeCookie] = useCookies(["token"]);
   const [user, setUser] = useState({});
   const [showLogout, setShowLogout] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 769) {
+      setMenuOpen(false);
+    }
+  }, [size.width]);
 
   const validateToken = async () => {
     const responseToken = await checkToken(cookies.token);
@@ -27,70 +56,93 @@ const NavMenu = () => {
     }
   }, [cookies?.token]);
 
+  const linkClickHandler = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <div className="menu">
-      <NavLink
-        to={"/"}
-        className={({ isActive }) =>
-          `menu__link  ${isActive ? "menu__link--active" : ""}`
-        }
+    <div className="header-wrapper">
+      <div
+        className={`menu ${menuOpen ? "menu--isMenu" : "menu--withoutMenu"}`}
       >
-        HOME
-      </NavLink>
-      <NavLink
-        to={"/poetry"}
-        className={({ isActive }) =>
-          `menu__link  ${isActive ? "menu__link--active" : ""}`
-        }
-      >
-        POETRY
-      </NavLink>
-      <NavLink
-        to={"/about"}
-        className={({ isActive }) =>
-          `menu__link  ${isActive ? "menu__link--active" : ""}`
-        }
-      >
-        ABOUT US
-      </NavLink>
-      {Object.keys(user).length !== 0 ? (
-        <div
-          className="menu__account"
-          onMouseEnter={() => setShowLogout(true)}
-          onMouseLeave={() => setShowLogout(false)}
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            `menu__link  ${isActive ? "menu__link--active" : ""}`
+          }
+          onClick={linkClickHandler}
         >
+          HOME
+        </NavLink>
+        <NavLink
+          to={"/poetry"}
+          className={({ isActive }) =>
+            `menu__link  ${isActive ? "menu__link--active" : ""}`
+          }
+          onClick={linkClickHandler}
+        >
+          POETRY
+        </NavLink>
+        <NavLink
+          to={"/about"}
+          className={({ isActive }) =>
+            `menu__link  ${isActive ? "menu__link--active" : ""}`
+          }
+          onClick={linkClickHandler}
+        >
+          ABOUT US
+        </NavLink>
+        {Object.keys(user).length !== 0 ? (
+          <div
+            className="menu__account"
+            onMouseEnter={() => setShowLogout(true)}
+            onMouseLeave={() => setShowLogout(false)}
+          >
+            <NavLink
+              to={"/account"}
+              className={({ isActive }) =>
+                `menu__link menu__link--account  ${
+                  isActive ? "menu__link--active" : ""
+                }`
+              }
+              onClick={linkClickHandler}
+            >
+              {user.photo !== "" ? (
+                <img src={user.photo} alt={user.nickName} />
+              ) : (
+                <img src={defaultUserPhoto} alt={user.nickName} />
+              )}
+            </NavLink>
+            {showLogout && (
+              <div className="menu__logout">
+                <button onClick={handleLogout}>Logout</button>
+              </div>
+            )}
+          </div>
+        ) : (
           <NavLink
-            to={"/account"}
+            to={"/login"}
             className={({ isActive }) =>
-              `menu__link menu__link--account  ${
+              `menu__link menu__link--login  ${
                 isActive ? "menu__link--active" : ""
               }`
             }
+            onClick={linkClickHandler}
           >
-            {user.photo !== "" ? (
-              <img src={user.photo} alt={user.nickName} />
-            ) : (
-              <img src={defaultUserPhoto} alt={user.nickName} />
-            )}
+            SIGN IN
           </NavLink>
-          {showLogout && (
-            <div className="menu__logout">
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-          )}
-        </div>
-      ) : (
-        <NavLink
-          to={"/login"}
-          className={({ isActive }) =>
-            `menu__link menu__link--login  ${
-              isActive ? "menu__link--active" : ""
-            }`
-          }
-        >
-          SIGN IN
-        </NavLink>
-      )}
+        )}
+      </div>
+      <div className="header-wrapper__menu">
+        {!menuOpen ? (
+          <BiMenuAltRight onClick={menuToggleHandler} />
+        ) : (
+          <AiOutlineClose
+            onClick={menuToggleHandler}
+            className="header-wrapper__close-icon"
+          />
+        )}
+      </div>
     </div>
   );
 };
